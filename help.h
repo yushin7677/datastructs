27fafa12ff674fcfcd90bf8c8de864f285c18a6d
yushin7677 <61585568+yushin7677@users.noreply.github.com>
2021-05-26 18:20:29 +0300

    Add files via upload

---
+++ help.h
@@ -0,0 +1,13 @@
+#ifndef _HELP_H
+#define _HELP_H
+#endif
+
+int min(int a, int b){
+	if(a < b) return a;
+	else return b;
+};
+
+int max(int a, int b){
+	if(a > b) return a;
+	else return b;
+};
---
+++ main.c
@@ -0,0 +1,19 @@
+﻿#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "array.h"
+
+int main(){
+
+	srand(time(NULL));
+	Array arr;
+	for(int i = 0; i < 100; i++){
+		pushIntoEnd(&arr, rand()%100);
+	};
+	printArray(&arr);
+	printf("\n1111\n\n");
+	mergeSort(&arr);
+	printArray(&arr);
+
+};
